3be0ba544b806d262ebdd675b8ad7e3bd1391c59
Vasco Pacheco <vascopacheco@MBP-de-Vasco.lan>
2020-12-21 16:16:52 +0000

    Chamada de protocolo e numero de pacotes enviados através de linha de comandos

--- file.py
+++ file.py
@@ -15,18 +15,41 @@ parser = argparse.ArgumentParser()
 
 # Adding optional argument
 parser.add_argument("-i", "--ip", help = "Destination IP Address")
+parser.add_argument("-p", "--protocol", help = "Protocol name")
+parser.add_argument("-n", "--number", help = "Number of packets")
+
 
 # Read arguments from command line
 args = parser.parse_args()
  
+print (args)
 if args.ip:
-    print("Diplaying IP as: % s" % args.ip)
-    ip_layer = IP(dst=args.ip)
-        
-    layer = ICMP(seq=9999)
-    packet = ip_layer / layer
+	print("Diplaying IP as: % s" % args.ip)
+	ip_layer = IP(dst=args.ip)
 
-    # loop=1 send packets until CTRL-C is pressed
-    send(packet, loop=1)
+	#layer = ICMP()
+	#packet = ip_layer / layer
+
+	# loop=1 send packets until CTRL-C is pressed
+	#send(packet, loop=1)
+
+	if args.protocol:
+		print("Diplaying protocol as: % s" % args.protocol)
+
+		layer = eval(args.protocol.upper() + "()")
+
+	else:
+		layer = ICMP()
+
+	packet = ip_layer / layer
+
+	if args.number:
+		send(packet, count=int(args.number))
+
+	else:
+		send(packet, loop=1)
+
+else:
+	print("No args")
 
 
