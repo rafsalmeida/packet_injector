5f67435bd03c46324064b29f1d1b190e6043e497
rafsalmeida <rr.pataias@gmail.com>
2020-12-22 16:07:53 +0000

    Adding the possibility to choose the source IP address

--- file.py
+++ file.py
@@ -3,18 +3,12 @@
 from scapy.all import *
 import argparse
 
-
-#example to see if it works, worked with me (verified with wireshark)
-#ip_layer = IP(dst="192.168.1.1")
-#icmp_layer = ICMP(seq=9999)
-#packet = ip_layer / icmp_layer
-#send(packet)
-
 # Initialize parser
 parser = argparse.ArgumentParser()
 
 # Adding optional argument
 parser.add_argument("-i", "--ip", help = "Destination IP Address")
+parser.add_argument("-s", "--source", help = "Source IP Address")
 parser.add_argument("-p", "--protocol", help = "Protocol name")
 parser.add_argument("-n", "--number", help = "Number of packets")
 
@@ -25,13 +19,12 @@ args = parser.parse_args()
 print (args)
 if args.ip:
 	print("Diplaying IP as: % s" % args.ip)
-	ip_layer = IP(dst=args.ip)
 
-	#layer = ICMP()
-	#packet = ip_layer / layer
-
-	# loop=1 send packets until CTRL-C is pressed
-	#send(packet, loop=1)
+	if args.source:
+		print("Diplaying source as: % s" % args.source)
+		ip_layer = IP(dst=args.ip, src=args.source)
+	else:
+		ip_layer = IP(dst=args.ip)
 
 	if args.protocol:
 		print("Diplaying protocol as: % s" % args.protocol)
@@ -50,6 +43,6 @@ if args.ip:
 		send(packet, loop=1)
 
 else:
-	print("No args")
+	print("Defining one/multiple destination IP Addresses is mandatory.")
 
 
