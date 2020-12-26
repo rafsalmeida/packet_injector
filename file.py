f3544ac301d73254d2aa34db682742b549675618
rafsalmeida <rr.pataias@gmail.com>
2020-12-26 16:43:46 +0000

    Read ip addresses from a file and sending packets

--- file.py
+++ file.py
@@ -11,6 +11,7 @@ parser.add_argument("-i", "--ip", help = "Destination IP Address")
 parser.add_argument("-s", "--source", help = "Source IP Address")
 parser.add_argument("-p", "--protocol", help = "Protocol name")
 parser.add_argument("-n", "--number", help = "Number of packets")
+parser.add_argument("-f", "--file", type=argparse.FileType('r'), help = "File with destination IP Addresses")
 
 
 # Read arguments from command line
@@ -43,6 +44,44 @@ if args.ip:
 		send(packet, loop=1)
 
 else:
-	print("Defining one/multiple destination IP Addresses is mandatory.")
+	if args.file:
+		Lines = args.file.readlines()
+
+		#TODO transform the file to have an address in each line (Strip by space e.g)
+
+		#TODO do this with threads
+
+		#TODO argument validation 
+
+		# Strips the newline character 
+		for line in Lines: 
+			print("Line:", line.strip())
+
+			if args.source:
+				print("Diplaying source as: % s" % args.source)
+				ip_layer = IP(dst=line.strip(), src=args.source)
+			else:
+				ip_layer = IP(dst=line.strip())
+
+			if args.protocol:
+				print("Diplaying protocol as: % s" % args.protocol)
+
+				layer = eval(args.protocol.upper() + "()")
+
+			else:
+				layer = ICMP()
+
+			packet = ip_layer / layer
+
+			if args.number:
+				send(packet, count=int(args.number))
+
+			else:
+				send(packet, loop=1)
+				
+	
+		
+	else:
+		print("Defining one/multiple destination IP Addresses is mandatory.")
 
 
